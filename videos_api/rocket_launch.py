3181354c50cdf90e5eff7551278b770143f4908b
André Dias <andremssdias@hotmail.com>
2023-02-17 00:58:23 +0100

    Create RocketLaunch API.

---
+++ videos_api/rocket_launch.py
@@ -0,0 +1,13 @@
+import requests
+
+
+class RocketLaunch:
+    def __init__(self, frames, url):
+        self.frames = frames
+        self.url = url
+
+    def get_frame(self, frame):
+        url_formated = f"{self.url}frame/{frame}/"
+
+        response = requests.get(url_formated)
+        return response.content if response.status_code == 200 else None

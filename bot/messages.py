52c1f110bca357f1206f069d1b24cbe7ea4e755e
André Dias <andremssdias@hotmail.com>
2023-02-17 15:51:05 +0100

    Create bot welcome message.

--- bot/messages.py
+++ bot/messages.py
@@ -1,16 +1,33 @@
 import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
-from settings import BOT_TOKEN
+from settings import BOT_TOKEN, VIDEO_URL
+from videos_api.rocket_launch import RocketLaunch
+from videos_api.utils import get_video_info
 
 
-bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
+class RocketBot:
+    def __init__(self, token):
+        self.bot = telebot.TeleBot(token, parse_mode=None)
 
+        rl_info = get_video_info(VIDEO_URL)
+        self.rocket = RocketLaunch(frames=rl_info["frames"], url=rl_info["url"])
 
-@bot.message_handler(commands=["start"])
-def send_welcome(message):
-    first_name = message.from_user.first_name
-    bot.reply_to(message, f"Hey, {first_name}!")
-    bot.send_message(
-        chat_id=message.chat.id,
-        text=f"I need your help to discover the exact frame where a rocket got launched. (You can check it by watching the picture on the top right corner)",
-    )
+        self.keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+        self.keyboard.add(KeyboardButton("Yes"), KeyboardButton("No"))
+
+        self.min_frame = 0
+        self.max_frame = rl_info["frames"]
+        self.current_frame = rl_info["frames"] // 2
+
+    def start(self):
+        @self.bot.message_handler(commands=["start"])
+        def send_welcome(message) -> None:
+            first_name = message.from_user.first_name
+            self.bot.reply_to(message, f"Hey, {first_name}!")
+            self.bot.send_message(
+                chat_id=message.chat.id,
+                text=f"I need your help to discover the exact frame where a rocket got launched. (You can check it by watching the picture on the top right corner)",
+            )
+
+        self.bot.infinity_polling()

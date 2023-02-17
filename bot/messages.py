0dd83d145fc9c3ed91748d03d14730e5eae3d3a2
André Dias <andremssdias@hotmail.com>
2023-02-17 16:11:39 +0100

    Create bot process answer and binary search algorithm.

--- bot/messages.py
+++ bot/messages.py
@@ -1,3 +1,4 @@
+import io
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
@@ -30,4 +31,58 @@ class RocketBot:
                 text=f"I need your help to discover the exact frame where a rocket got launched. (You can check it by watching the picture on the top right corner)",
             )
 
+            img = self.rocket.get_rocket_launch_frame(self.current_frame)
+            if img:
+                self.send_rocket_img(img=img, chat_id=message.chat.id)
+
+        @self.bot.message_handler(func=lambda msg: True)
+        def process_answer(message) -> None:
+            if (
+                self.current_frame + 1 == self.max_frame
+                or self.current_frame - 1 == self.min_frame
+            ):
+                frame = self.max_frame if self.current_frame + 1 == self.max_frame else self.min_frame
+                
+                self.bot.send_message(
+                    chat_id=message.chat.id,
+                    text=f"You found it! The frame is {frame}",
+                )
+
+                return None
+
+            if message.text.upper() in ["YES", "Y"]:
+                # Go back on frames
+
+                self.max_frame = self.current_frame + 1
+                self.current_frame = (self.min_frame + self.max_frame) // 2
+
+                img = self.rocket.get_rocket_launch_frame(self.current_frame)
+                if img:
+                    self.send_rocket_img(img=img, chat_id=message.chat.id)
+
+            elif message.text.upper() in ["NO", "N"]:
+                # Go front on frames
+
+                self.min_frame = self.current_frame - 1
+                self.current_frame = (self.min_frame + self.max_frame) // 2
+
+                img = self.rocket.get_rocket_launch_frame(self.current_frame)
+                if img:
+                    self.send_rocket_img(img=img, chat_id=message.chat.id)
+
+            else:
+                self.bot.reply_to(
+                    message, "I'm sorry, I didn't understand your response."
+                )
+
         self.bot.infinity_polling()
+
+    def send_rocket_img(self, img, chat_id, text="Did the rocket launched?"):
+        photo = io.BytesIO(img)
+        self.bot.send_photo(chat_id=chat_id, photo=photo)
+
+        self.bot.send_message(
+            chat_id=chat_id,
+            text=text,
+            reply_markup=self.keyboard,
+        )

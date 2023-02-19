c348dcff3d64d426284e34852d7e040138f585d3
André Dias <andremssdias@hotmail.com>
2023-02-19 14:32:42 +0100

    Change bot message handlers to methods.
    In this commit:
    - Changed message handlers for methos, for easier testing.
    - Validate that user found frame if answered correctly to 'yes' or 'no'.

--- bot/rocket_bot.py
+++ bot/rocket_bot.py
@@ -24,94 +24,103 @@ class RocketBot:
     def start(self):
         @self.bot.message_handler(commands=["start"])
         def send_welcome(message) -> None:
-            chat_id = message.chat.id
-            first_name = message.from_user.first_name
-
-            # Check if user exists, if yes, ask him to help you again, else save user info
-            if self.db.user_exists(chat_id):
-                user_frames_info = self.db.get_user_info(chat_id)
-                self.bot.send_message(
-                    chat_id=chat_id,
-                    text=f"Hey {first_name}! Can you continue helping me discover when was the exact frame a rocket launched? (You can check it by watching the picture on the top right corner)",
-                )
-                self.bot.send_message(
-                    chat_id=chat_id,
-                    text="If you would like to restart from beginning just type /restart.",
-                )
-
-            else:
-                user_frames_info = self.db.create_user(
-                    chat_id=chat_id, max_frame=self.rocket.frames
-                )
-
-                self.bot.reply_to(message, f"Hey, {first_name}!")
-                self.bot.send_message(
-                    chat_id=chat_id,
-                    text=f"I need your help to discover the exact frame where a rocket got launched. (You can check it by watching the picture on the top right corner)",
-                )
-
-            self.send_rocket_image(
-                chat_id=chat_id, current_frame=user_frames_info["current_frame"]
-            )
+            self.send_welcome(message)
 
         @self.bot.message_handler(commands=["restart"])
         def restart(message) -> None:
-            chat_id = message.chat.id
-            self.db.delete_user(chat_id)
-            self.bot.send_message(
-                chat_id=chat_id,
-                text="Great. To start again type /start.",
-            )
+            self.restart(message)
 
         @self.bot.message_handler(func=lambda msg: True)
         def process_answer(message) -> None:
-            chat_id = message.chat.id
-
-            if not self.db.user_exists(chat_id=chat_id):
-                self.bot.send_message(
-                    chat_id=chat_id,
-                    text=f"To start, type /start.",
-                )
-                return None
-
-            user_frames = self.db.get_user_info(chat_id=chat_id)
-            user_message = message.text.upper()
-
-            if self.found_frame(user_frames):
-                frame = (
-                    user_frames["max_frame"]
-                    if user_frames["current_frame"] == user_frames["min_frame"]
-                    and user_message in ["NO", "N"]
-                    else user_frames["min_frame"]
-                )
-
-                self.bot.send_message(
-                    chat_id=chat_id,
-                    text=f"You found it! The frame is {frame}. To do it again type /start.",
-                )
-                self.db.delete_user(chat_id)
-
-                return None
-
-            if user_message in ["YES", "Y", "NO", "N"]:
-                # Go backwards frames if True
-                backwards = True if user_message in ["YES", "Y"] else False
-                new_frames = self.update_user_frames(
-                    chat_id=chat_id, user_frames=user_frames, backwards=backwards
-                )
-
-                # Send rocket image with new frames
-                self.send_rocket_image(
-                    chat_id=chat_id, current_frame=new_frames["current_frame"]
-                )
-
-            else:
-                self.bot.reply_to(
-                    message,
-                    "I'm sorry, I didn't understand your response. Just type 'Yes' or 'No'.",
-                )
+            self.process_answer(message)
 
         self.bot.infinity_polling()
+
+    def send_welcome(self, message) -> None:
+        chat_id = message.chat.id
+        first_name = message.from_user.first_name
+
+        # Check if user exists, if yes, ask him to help you again, else save user info
+        if self.db.user_exists(chat_id):
+            user_frames_info = self.db.get_user_info(chat_id)
+            self.bot.send_message(
+                chat_id=chat_id,
+                text=f"Hey {first_name}! Can you continue helping me discover when was the exact frame a rocket launched? (You can check it by watching the picture on the top right corner)",
+            )
+            self.bot.send_message(
+                chat_id=chat_id,
+                text="If you would like to restart from beginning just type /restart.",
+            )
+
+        else:
+            user_frames_info = self.db.create_user(
+                chat_id=chat_id, max_frame=self.rocket.frames
+            )
+
+            self.bot.reply_to(message, f"Hey, {first_name}!")
+            self.bot.send_message(
+                chat_id=chat_id,
+                text=f"I need your help to discover the exact frame where a rocket got launched. (You can check it by watching the picture on the top right corner)",
+            )
+
+        self.send_rocket_image(
+            chat_id=chat_id, current_frame=user_frames_info["current_frame"]
+        )
+
+    def restart(self, message) -> None:
+        chat_id = message.chat.id
+        self.db.delete_user(chat_id)
+        self.bot.send_message(
+            chat_id=chat_id,
+            text="Great. To start again type /start.",
+        )
+
+    def process_answer(self, message) -> None:
+        chat_id = message.chat.id
+
+        if not self.db.user_exists(chat_id=chat_id):
+            self.bot.send_message(
+                chat_id=chat_id,
+                text=f"To start, type /start.",
+            )
+            return None
+
+        user_frames = self.db.get_user_info(chat_id=chat_id)
+        user_message = message.text.upper()
+
+        if self.found_frame(user_frames) and user_message in ["YES", "Y", "NO", "N"]:
+            frame = (
+                user_frames["max_frame"]
+                if user_frames["current_frame"] == user_frames["min_frame"]
+                and user_message in ["NO", "N"]
+                else user_frames["min_frame"]
+            )
+
+            self.bot.send_message(
+                chat_id=chat_id,
+                text=f"You found it! The frame is {frame}. To do it again type /start.",
+            )
+            self.db.delete_user(chat_id)
+
+            return None
+
+        if user_message in ["YES", "Y", "NO", "N"]:
+            # Go backwards frames if True
+            backwards = True if user_message in ["YES", "Y"] else False
+            new_frames = self.update_user_frames(
+                chat_id=chat_id, user_frames=user_frames, backwards=backwards
+            )
+
+            # Send rocket image with new frames
+            self.send_rocket_image(
+                chat_id=chat_id, current_frame=new_frames["current_frame"]
+            )
+
+        else:
+            self.bot.reply_to(
+                message,
+                "I'm sorry, I didn't understand your response. Just type 'Yes' or 'No'.",
+            )
 
     def update_user_frames(
         self, chat_id: int, user_frames: Dict, backwards: bool

fc99f460e4df99c42a53698e5d0eda1674e455a3
André Dias <andremssdias@hotmail.com>
2023-02-21 00:45:11 +0100

    Add sentry configuration.

--- settings/settings.py
+++ settings/settings.py
@@ -2,6 +2,9 @@ import logging
 import os
 
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
+
 
 load_dotenv()
 
@@ -15,6 +18,20 @@ logger = logging.getLogger("telegram-bot-logger")
 logger.setLevel(logging.CRITICAL)
 formatter = logging.Formatter("%(levelname)-8s %(asctime)s: %(message)s - %(module)s - %(funcName)s - %(lineno)d")
 logging.basicConfig(format="%(levelname)-8s %(asctime)s: %(message)s - %(module)s - %(funcName)s - %(lineno)d")
+
+# Sentry config for logging
+sentry_logging = LoggingIntegration(
+    level=logging.INFO,        # Capture info and above as breadcrumbs
+    event_level=logging.INFO  # Send errors as events
+)
+sentry_sdk.init(
+    dsn=os.environ.get("DSN", ""),
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
 
 
 if DEBUG:
